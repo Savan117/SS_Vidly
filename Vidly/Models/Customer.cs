a434eab1dec8527a30c44cda68fa752dc0cf3851
Savan Scott <okamiscot@hotmail.com>
2018-06-19 14:16:17 -0500

    second commit , done migrations and added  customer and  membershiptype tables.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,9 +5,12 @@ using System.Web;
 
 namespace Vidly.Models
 {
-    public class Customer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool IsSubscribedToNewsletter { get; set; }
+		public MembershipType MembershipType { get; set; }
+		public byte MembershipTypeID { get; set; }
+	}
 }
